9931618f67acfcbabe579e0293a946ecb099d54c
jose <joseluis19963@gmail.com>
2020-10-15 02:06:09 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'foro-angular';
+  title = 'Foro en Angular';
 }
---
+++ src/app/models/comment.ts
@@ -0,0 +1,10 @@
+export class Comment {
+  constructor(
+    public _id: string,
+    public content: string,
+    public date: string,
+    public user: any
+  ) {
+
+  }
+}
